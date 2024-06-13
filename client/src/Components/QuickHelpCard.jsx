4484fadc06e9861c950d23dbba136f1b84cac95d
biswarup-naha <jeeeeet6902@gmail.com>
2024-06-14 00:22:18 +0530

    FAQs comp added and quick-help cards styled up

--- client/src/Components/QuickHelpCard.jsx
+++ client/src/Components/QuickHelpCard.jsx
@@ -1,16 +1,17 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-const QuickHelpCard = ({data, link}) => {
-    const service=data;
+const QuickHelpCard = ({ data, link }) => {
+  const service = data;
+
   return (
-      <Link to={link}><div className="flex bg-white box-border h-60 w-52 rounded-3xl p-4 border-4 shadow-[0_24px_40px_-15px_rgba(0,0,0,0.3)] place-content-center items-center mx-14 my-10">
-      <p className="text-black font-semibold text-2xl mt-2">
-        {service}
-      </p>
-    </div>
+    <Link to={link}>
+      <div className="relative flex bg-white box-border h-60 w-52 rounded-3xl p-4 border-4 border-gray-300 shadow-lg place-content-center items-center mx-14 my-10 transform transition-transform duration-300 hover:scale-105 hover:shadow-2xl hover:border-teal-500 hover:bg-teal-50">
+        <p className="text-black font-semibold text-2xl mt-2 text-center">
+          {service}
+        </p>
+      </div>
     </Link>
-    
   );
 };
 
