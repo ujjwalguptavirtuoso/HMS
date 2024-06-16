4c2a99f2ebbf8c472e94d3682e1c95d1e4842078
Aayush Chatterjee <99548784+aayush8421@users.noreply.github.com>
2024-06-17 00:46:54 +0530

    more minor fixes

--- client/src/Components/AppointDoctors.jsx
+++ client/src/Components/AppointDoctors.jsx
@@ -17,7 +17,7 @@ const AppointDoctors = ({ data, onClick }) => {
       className="flex  bg-white box-border h-fit w-52 rounded-3xl p-4 border-4 shadow-[0_24px_40px_-15px_rgba(0,0,0,0.3)] flex-col items-center mx-14 mb-10"
     >
       <div className="w-28 h-28 rounded-full border-2 border-emerald-300 mb-2">
-        <img className="w-28 h-28 rounded-full" src={data.avatar.url} alt="" />
+        <img className="w-28 h-28 rounded-full" src={data.avatar && data.avatar.url} alt="" />
       </div>
       <h1 className="text-black font-semibold text-xl ">
         {data.firstName} {data.lastName}
