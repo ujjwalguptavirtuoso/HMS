3337ed415b7dc8f5640129de52ce1745164d9325
Ujjwal Gupta <ujjwalgupta@Ujjwals-MacBook-Pro.local>
2025-03-31 11:02:19 -0400

    changes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,7 +4,6 @@ import "react-toastify/dist/ReactToastify.css";
 import Home from "./Pages/Home";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Appointment from "./Pages/Appointment";
-import About from "./Pages/About";
 import QuickHelp from "./Pages/QuickHelp";
 import Login from "./Pages/login";
 import Register from "./Pages/Register";
@@ -55,7 +54,6 @@ const App = () => {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/appointment" element={<Appointment />} />
-          <Route path="/aboutus" element={<About />} />
           <Route path="/quick-help" element={<QuickHelp />} />
           <Route path="/diabetes-predictor" element={<DiabetesPredictor />} />
           <Route path="/thyroid-predictor" element={<ThyroidPredictor />} />
